ea154896d43e167986129aacb7c23d429450e32b
alex <Sasha21031997@icloud.com>
2017-12-10 23:49:12 +0300

    Add UserSerializer

--- wordfashrest/api/serializers.py
+++ wordfashrest/api/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Dictionary
 
@@ -9,3 +10,13 @@ class DictionarySerializer(serializers.ModelSerializer):
         """Meta class to map serializer's fields with the model fields."""
         model = Dictionary
         fields = ('id', 'word', 'description')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer to map the model instance into json format."""
+
+    class Meta:
+        """Map this serializer to a model and their fields."""
+        model = User
+        fields = ('id', 'username', 'password', 'profile.history_words', 'profile.favorite_words')
+        read_only_fields = ('profile.history_words', 'profile.favorite_words')
