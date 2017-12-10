abf998a5787f0825495290419d2b3f096812cefe
alex <Sasha21031997@icloud.com>
2017-12-10 16:39:09 +0300

    Add CreateView

--- wordfashrest/api/views.py
+++ wordfashrest/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import DictionarySerializer
+from .models import Dictionary
 
-# Create your views here.
+
+class CreateView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = Dictionary.objects.all()
+    serializer_class = DictionarySerializer
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new dictionary."""
+        serializer.save()
