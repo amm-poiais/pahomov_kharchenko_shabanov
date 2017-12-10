4cc58bb36a4decc4ffb4374b2ace6982101f5183
alex <Sasha21031997@icloud.com>
2017-12-10 16:46:49 +0300

    Change path to url, cause it did't work before

--- wordfashrest/wordfashrest/urls.py
+++ wordfashrest/wordfashrest/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
+from django.conf.urls import url, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^', include('api.urls'))
+    url(r'^', include('api.urls'))
 ]
