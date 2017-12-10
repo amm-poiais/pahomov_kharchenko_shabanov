e632107e7ac100f4aeb0b6ebb711abc4c573d4a9
alex <Sasha21031997@icloud.com>
2017-12-10 18:29:11 +0300

    Make test passable

--- wordfashrest/api/tests.py
+++ wordfashrest/api/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from .models import Dictionary
 from rest_framework.test import APIClient
 from rest_framework import status
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 
 class ModelTestCase(TestCase):
@@ -50,11 +50,12 @@ class ViewTestCase(TestCase):
     def test_api_can_update_dictionary(self):
         """Test the api can update a given dictionary."""
         change_dictionary = {'word': 'Test', 'description': 'New test description'}
-        dictionary = Dictionary.objects.get(word=change_dictionary.word)
+        dictionary = Dictionary.objects.get(id=1)
         res = self.client.put(
             reverse('details', kwargs={'pk': dictionary.id}),
             change_dictionary, format='json'
         )
+
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
     def test_api_can_delete_bucketlist(self):
