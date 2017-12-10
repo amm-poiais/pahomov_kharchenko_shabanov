c48a411792ca1601e4032e290e7b0c490d2202d8
alex <Sasha21031997@icloud.com>
2017-12-10 16:58:37 +0300

    Add test cases for update, delete and get methods(dictionary)

--- wordfashrest/api/tests.py
+++ wordfashrest/api/tests.py
@@ -36,3 +36,33 @@ class ViewTestCase(TestCase):
     def test_api_can_create_a_dictionary(self):
         """Test the api has dictionary creation capability."""
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_api_can_get_a_dictionary(self):
+        """Test the api can get a given dictionary."""
+        dictionary = Dictionary.objects.get()
+        response = self.client.get(
+            reverse('details',
+                    kwargs={'pk': dictionary.id}), format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, dictionary)
+
+    def test_api_can_update_dictionary(self):
+        """Test the api can update a given dictionary."""
+        change_dictionary = {'word': 'Test', 'description': 'New test description'}
+        dictionary = Dictionary.objects.get(word=change_dictionary.word)
+        res = self.client.put(
+            reverse('details', kwargs={'pk': dictionary.id}),
+            change_dictionary, format='json'
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_bucketlist(self):
+        """Test the api can delete a dictionary."""
+        dictionary = Dictionary.objects.get()
+        response = self.client.delete(
+            reverse('details', kwargs={'pk': dictionary.id}),
+            format='json',
+            follow=True)
+
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
