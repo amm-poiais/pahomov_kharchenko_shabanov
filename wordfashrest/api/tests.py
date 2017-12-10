d5975e3ff99d8b403743fa3e5326735577216ae4
alex <Sasha21031997@icloud.com>
2017-12-10 16:36:18 +0300

    Create test case for dictionary view(post)

--- wordfashrest/api/tests.py
+++ wordfashrest/api/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from .models import Dictionary
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.core.urlresolvers import reverse
 
 
 class ModelTestCase(TestCase):
@@ -17,3 +20,19 @@ class ModelTestCase(TestCase):
         new_count = Dictionary.objects.count()
         self.assertNotEqual(old_count, new_count)
 
+
+class ViewTestCase(TestCase):
+    """Test suite for the api views."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.client = APIClient()
+        self.dictionary_data = {'word': 'Test', 'description': 'Test description'}
+        self.response = self.client.post(
+            reverse('create'),
+            self.dictionary_data,
+            format="json")
+
+    def test_api_can_create_a_dictionary(self):
+        """Test the api has dictionary creation capability."""
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
