c3e0b8bf964ca3e1d77a952461f2493c7a5f5efd
alex <Sasha21031997@icloud.com>
2017-12-18 17:54:27 +0300

    Add get and post methods to "user/words/" route
    It work good, we can add history and favorite words to user
    and get all information about him.

--- wordfashrest/api/urls.py
+++ wordfashrest/api/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CreateDictionaryView,  UserDetailsView, CreateUserView
+from .views import CreateDictionaryView,  UserDetail, CreateUserView
 from .views import DetailsDictionaryView
 
 
@@ -10,8 +10,8 @@ urlpatterns = {
     url(r'^words/$', CreateDictionaryView.as_view(), name="create"),
     url(r'^word/(?P<pk>[0-9]+)/$', DetailsDictionaryView.as_view(), name="details"),
 #    url(r'^user/$', UserCreateView.as_view(), name="create"),
-    url(r'^users/(?P<pk>[0-9]+)/$', UserDetailsView.as_view(), name="details"),
-    url(r'^user/words/$', UserDetailsView.as_view(), name="details"),
+#    url(r'^users/(?P<pk>[0-9]+)/$', UserDetailsView.as_view(), name="details"),
+    url(r'^user/words/$', UserDetail.as_view(), name="details"),
     url(r'^get-token/', obtain_auth_token),
     url(r'^register/$', CreateUserView.as_view())
 }
--- wordfashrest/api/views.py
+++ wordfashrest/api/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import User
-from rest_framework import generics
+from django.http import Http404
+from rest_framework import generics, status
 from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .permissions import IsUser
 from .serializers import DictionarySerializer, UserSerializer
@@ -31,19 +34,68 @@ class DetailsDictionaryView(generics.RetrieveUpdateDestroyAPIView):
         IsUser)
 
 
-class UserDetailsView(generics.RetrieveUpdateDestroyAPIView):
-    """This class handles the http GET, PUT and DELETE requests."""
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = (
-        permissions.IsAuthenticated,
-        IsUser)
+# class UserDetailsView(generics.RetrieveUpdateDestroyAPIView):
+#     """This class handles the http GET, PUT and DELETE requests."""
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#     permission_classes = (
+#         permissions.IsAuthenticated,
+#         IsUser)
+#
+#     def post(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+#
+#     def filter_queryset(self, queryset):
+#         return self.request.user
 
-    def post(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
 
-    def filter_queryset(self, queryset):
-        return self.request.user
+class UserDetail(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+
+    def get_object(self):
+        try:
+            return User.objects.get(pk=self.request.user.id)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request):
+        user = self.get_object()
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def post(self, request):
+        user = self.get_object()
+        try:
+            history_words = request.data["history"].split(';')
+            favorite_words = request.data["favorite"].split(';')
+
+            user.profile.history_words.clear()
+            for word in history_words:
+                user.profile.history_words.add(
+                    Dictionary.objects.get(word=word)
+                )
+
+            user.profile.favorite_words.clear()
+            for word in favorite_words:
+                user.profile.favorite_words.add(
+                    Dictionary.objects.get(word=word)
+                )
+
+            user.save()
+
+        except Exception as ex:
+             return Response(ex, status=status.HTTP_400_BAD_REQUEST)
+
+        #return Response(UserSerializer(user).data)
+        return Response(status=status.HTTP_200_OK)
+
+
+    def delete(self):
+        user = self.get_object()
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class CreateUserView(CreateAPIView):
@@ -52,7 +104,6 @@ class CreateUserView(CreateAPIView):
         permissions.AllowAny
     ]
     serializer_class = UserSerializer
-
 
 # class UserCreateView(generics.ListCreateAPIView):
 #     """This class defines the create behavior of our rest api."""
