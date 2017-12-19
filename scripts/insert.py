bdade6b9d0b9f48dbdb4646a4e30753a18bacc72
alex <Sasha21031997@icloud.com>
2017-12-19 13:33:24 +0300

    Add host to db connection

--- scripts/insert.py
+++ scripts/insert.py
@@ -7,7 +7,12 @@ import psycopg2
 #import sqlite3
 #db = sqlite3.connect('../db.sqlite3')
 
-db = psycopg2.connect("dbname=d13o8ji4kob0as user=ckyuocextchdv password=abd7e1aa626733780addaeeda9c4fd00cfb08f4b87b177eac05db58afbfb6e2e")
+db = psycopg2.connect(
+    host="ec2-23-21-236-249.compute-1.amazonaws.com",
+    dbname="d13o8ji4kob0as",
+    user="ckyuocextchdv",
+    password="abd7e1aa626733780addaeeda9c4fd00cfb08f4b87b177eac05db58afbfb6e2e"
+                      )
 dictionary = json.load(open('dictionary.json'))
 
 query = "insert into api_dictionary values (?,?)"
